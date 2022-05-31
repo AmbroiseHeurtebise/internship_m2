df9cab3c262d73132f81693bf70437d82651e2c6
Pierre Ablin <pierreablin@gmail.com>
2022-05-31 14:35:40 +0200

    formating

--- plot_camcan_late_early.py
+++ plot_camcan_late_early.py
@@ -17,19 +17,18 @@ s2 = results["sources2"]
 
 # correct sign
 
-s1 *= 2 * (s1.max(axis=0) > - s1.min(axis=0)) - 1
-s2 *= 2 * (s2.max(axis=0) > - s2.min(axis=0)) - 1
+s1 *= 2 * (s1.max(axis=0) > -s1.min(axis=0)) - 1
+s2 *= 2 * (s2.max(axis=0) > -s2.min(axis=0)) - 1
 
 source_indexes = [2, 4]
 
 plt.figure(figsize=(3, 2))
 
 n, _ = s1.shape
-time = np.linspace(-.2, .5, n)
-for sources, index, name in zip([s1, s2], source_indexes, ['Early', 'Late']):
+time = np.linspace(-0.2, 0.5, n)
+for sources, index, name in zip([s1, s2], source_indexes, ["Early", "Late"]):
     plt.plot(time, sources[:, index], label=name)
 plt.legend()
-x_ = plt.xlabel('Time (s.)')
-y_ = plt.ylabel('Amplitude')
-plt.savefig('camcan_late_early.pdf', bbox_extra_artists=[x_, y_], bbox_inches='tight')
-
+x_ = plt.xlabel("Time (s.)")
+y_ = plt.ylabel("Amplitude")
+plt.savefig("camcan_late_early.pdf", bbox_extra_artists=[x_, y_], bbox_inches="tight")
--- run_camcan_late_early.py
+++ run_camcan_late_early.py
@@ -4,7 +4,7 @@ import numpy as np
 from mvlearn.decomposition import MultiviewICA
 
 
-X = np.load('evoked_data_camcan.npy')  ## Camcan data
+X = np.load("evoked_data_camcan.npy")  ## Camcan data
 
 X /= np.mean(X ** 2, axis=(1, 2), keepdims=True)
 power = np.mean(X ** 2, axis=1)
@@ -19,6 +19,6 @@ n_sources = 10
 ica1 = MultiviewICA(n_sources, n_jobs=1).fit(X1.swapaxes(1, 2))
 ica2 = MultiviewICA(n_sources, n_jobs=1).fit(X2.swapaxes(1, 2))
 
-results = {'sources1': ica1.source_, 'sources2': ica2.source_}
+results = {"sources1": ica1.source_, "sources2": ica2.source_}
 with open("results_camcan_late_early.pkl", "wb") as save_results_file:
     pickle.dump(results, save_results_file)
