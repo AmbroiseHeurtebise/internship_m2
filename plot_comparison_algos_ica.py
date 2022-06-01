34e005f7821da4b71d7702d7ecabecb595846243
Ambroise Heurtebise <amheurtebise@gmail.com>
2022-06-01 10:25:52 +0200

    Modifying the plot file

--- plot_comparison_algos_ica.py
+++ plot_comparison_algos_ica.py
@@ -1,56 +1,7 @@
 import os
 import pickle
-import numpy as np
 import matplotlib.pyplot as plt
-
 import seaborn as sns
-
-# def plot_distances(delay, distances):
-#     eps = 1e-4
-
-#     # Median and std
-#     # mean_amari = np.mean(distances, axis=2)
-#     median_amari = np.median(distances, axis=2)
-#     std_amari = np.std(distances, axis=2)
-
-#     # Plot
-#     fig, ax = plt.subplots()
-#     ax.set_xlabel("Quantity of delay")
-#     ax.set_ylabel("Amari distance")
-
-#     ax.plot(delay - 1, median_amari[:, 0], label="MVICA", c="r")
-#     low_curve = median_amari[:, 0] - std_amari[:, 0]
-#     ax.fill_between(
-#         delay - 1,
-#         low_curve * (low_curve > 0) + eps * (low_curve <= 0),
-#         median_amari[:, 0] + std_amari[:, 0],
-#         color="r",
-#         alpha=0.1,
-#     )
-#     ax.plot(delay - 1, median_amari[:, 1], label="GroupICA", color="b")
-#     low_curve = median_amari[:, 1] - std_amari[:, 1]
-#     ax.fill_between(
-#         delay - 1,
-#         low_curve * (low_curve > 0) + eps * (low_curve <= 0),
-#         median_amari[:, 1] + std_amari[:, 1],
-#         color="b",
-#         alpha=0.1,
-#     )
-#     ax.plot(delay - 1, median_amari[:, 2], label="UniICA", color="orange")
-#     low_curve = median_amari[:, 2] - std_amari[:, 2]
-#     ax.fill_between(
-#         delay - 1,
-#         low_curve * (low_curve > 0) + eps * (low_curve <= 0),
-#         median_amari[:, 2] + std_amari[:, 2],
-#         color="orange",
-#         alpha=0.1,
-#     )
-
-#     ax.set_yscale("log")
-#     ax.legend()
-#     plt.title("Amari distance wrt the quantity of delay", fontweight="bold")
-#     plt.savefig("amari_delay_comparison.pdf")
-#     plt.show()
 
 
 if __name__ == "__main__":
@@ -62,24 +13,22 @@ if __name__ == "__main__":
     else:
         print("There isn't any source to plot \n")
 
-    sns.lineplot(data=results, x="Delay", y="Mean_Amari", hue="Algo")
-    plt.title("Amari distance wrt the quantity of delay", fontweight="bold")
-    plt.savefig("amari_delay_comparison.pdf")
+    # Plot
+    # sns.lineplot(data=results, x="Delay", y="Mean_Amari", hue="Algo")
+    # plt.title("Amari distance wrt the quantity of delay", fontweight="bold")
+    # plt.savefig("amari_delay_comparison.pdf")
+    # plt.show()
+    sns.set(font_scale=1.8)
+    sns.set_style("white")
+    sns.set_style('ticks')
+    fig = sns.lineplot(data=results, x="Delay", y="Mean_Amari", hue="Algo", linewidth=2.5)
+    fig.set(yscale='log')
+    x_ = plt.xlabel("Delay")
+    y_ = plt.ylabel("Reconstruction error")
+    leg = plt.legend(prop={'size': 20})
+    for line in leg.get_lines():
+        line.set_linewidth(2.5)
+    plt.grid()
+    plt.title("Reconstruction error wrt the delay", fontsize=18, fontweight="bold")
+    plt.savefig("amari_delay_comparison.pdf", bbox_extra_artists=[x_, y_], bbox_inches="tight")
     plt.show()
-
-    # # Get the delays and the distances
-    # delay = np.unique(results["Delay"])
-    # algos = np.unique(results["Algo"])
-    # random_states = np.unique(results["random_state"])
-
-    # distances = np.zeros((len(delay), len(algos), len(random_states)))
-    # for i, dd in enumerate(delay):
-    #     for j, aa in enumerate(algos):
-    #         distances[i, j, :] = np.asarray(
-    #             results.loc[
-    #                 (results["Delay"] == dd) & (results["Algo"] == aa)
-    #             ]["Mean_Amari"]
-    #         )
-
-    # # Plot
-    # plot_distances(delay, distances)
