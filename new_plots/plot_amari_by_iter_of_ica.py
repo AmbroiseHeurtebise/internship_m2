e5781f4257323acac3dbe9d5b6dc0db1dcb8450e
Ambroise Heurtebise <amheurtebise@gmail.com>
2022-09-21 10:24:12 +0200

    return_amari has become return_basis_list, these are the changes made accordingly

--- new_plots/plot_amari_by_iter_of_ica.py
+++ new_plots/plot_amari_by_iter_of_ica.py
@@ -1,5 +1,7 @@
+import numpy as np
 import matplotlib.pyplot as plt
-from delay_multiviewica import delay_multiviewica, multiviewica, create_sources_pierre
+from picard import amari_distance
+from delay_multiviewica import delay_multiviewica, multiviewica, create_sources_pierre, generate_data
 
 
 if __name__ == '__main__':
@@ -7,22 +9,38 @@ if __name__ == '__main__':
     m = 6
     p = 2
     n = 400
-    delay = 100
-    noise = 0.05
-    random_state = 20
+    nb_intervals = 4
+    nb_freqs = 5
+    treshold = 3
+    delay = 50
+    noise = 0.2
+    random_state = np.random.randint(1000)
+    print(random_state)
 
     # Generate data
-    X_list, A_list, _ = create_sources_pierre(
-        m, p, n, delay, sigma=noise, random_state=random_state)
+    # X_list, A_list, _ = create_sources_pierre(
+    #     m, p, n, delay, sigma=noise, random_state=random_state)
+    X_list, A_list, _, _, _ = generate_data(
+        m, p, n, nb_intervals=nb_intervals, nb_freqs=nb_freqs,
+        treshold=treshold, delay=delay, noise=noise, random_state=random_state)
 
     # Run ICA
-    _, _, _, _, amari_delay_mvica_earl_stop = delay_multiviewica(
-        X_list, early_stopping_delay=10, random_state=random_state, 
-        return_amari=True, A_list=A_list)
+    _, _, _, _, W_total_delay_mvica_earl_stop = delay_multiviewica(
+        X_list, early_stopping_delay=10, random_state=random_state,
+        return_basis_list=True)
 
-    _, _, _, amari_mvica = multiviewica(
-        X_list, random_state=random_state, return_amari=True, A_list=A_list)
-    
+    _, _, _, W_total_mvica = multiviewica(
+        X_list, random_state=random_state, return_basis_list=True)
+
+    # Compute Amari distance
+    amari_mvica = np.array([np.sum(
+        [amari_distance(W, A) for W, A in zip(W_list, A_list)])
+        for W_list in W_total_mvica])
+
+    amari_delay_mvica_earl_stop = np.array([np.sum(
+        [amari_distance(W, A) for W, A in zip(W_list, A_list)])
+        for W_list in W_total_delay_mvica_earl_stop])
+
     # Plot
     plt.plot(amari_delay_mvica_earl_stop, label='delay_mvica_earl_stop_10')
     plt.plot(amari_mvica, label='mvica')
