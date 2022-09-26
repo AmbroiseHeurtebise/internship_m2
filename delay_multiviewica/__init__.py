3d729ea57cc7af16dd52ceb490cda9087b613950
Ambroise Heurtebise <amheurtebise@gmail.com>
2022-09-26 15:02:13 +0200

    Add a function that optimizes the delays after permica but before find_ordering

--- delay_multiviewica/__init__.py
+++ delay_multiviewica/__init__.py
@@ -5,7 +5,7 @@ from ._delay_multiviewica import delay_multiviewica
 from ._multiviewica import multiviewica
 from ._groupica import groupica
 from ._permica import permica, _hungarian
-from .optimization_tau import _optimization_tau, _loss_delay, _apply_delay, _apply_delay_one_sub, _loss_delay_ref, _optimization_tau_approach1, _optimization_tau_approach2, _delay_estimation
+from .optimization_tau import _optimization_tau, _loss_delay, _apply_delay, _apply_delay_one_sub, _loss_delay_ref, _optimization_tau_approach1, _optimization_tau_approach2, _delay_estimation, delay_estimation_with_scale_perm
 from .sources_generation import _create_sources, create_sources_pierre, create_model, generate_data, plot_sources, _plot_delayed_sources
 from ._univiewica import univiewica
 
