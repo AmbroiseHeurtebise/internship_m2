ec8b2d6b74ccfc9f3839301f10bc17ec51498bae
AmbroiseHeurtebise <amheurtebise@gmail.com>
2023-03-17 15:15:01 +0100

    Also test _optimization_tau_by_source

--- multiviewica_delay/tests/test_loss_partial_decreasing.py
+++ multiviewica_delay/tests/test_loss_partial_decreasing.py
@@ -4,7 +4,9 @@ from multiviewica_delay import (
     _noisy_ica_step,
     _apply_delay,
     _apply_delay_one_sub,
-    _optimization_tau_approach1
+    _optimization_tau_approach1,
+    _apply_delay_by_source,
+    _optimization_tau_by_source
 )
 
 
@@ -18,17 +20,16 @@ def loss_partial(basis_list, Y_list, Y_avg, noise=1.0):
 
 
 @pytest.mark.parametrize(
-    "mode", ["low", "high"])
+    "mode", ["one_source", "multiple_sources"])
 def test_loss_partial_decreasing(mode):
     # parameters
-    if mode == "low":
-        m = 2
-        p = 3
-        n = 4
-    elif mode == "high":
-        m = 10
-        p = 20
-        n = 100
+    if mode == "one_source":
+        multiple_sources = False
+    elif mode == "multiple_sources":
+        multiple_sources = True
+    m = 10
+    p = 20
+    n = 100
     delay_max = 20
     seed = 0
     rng = np.random.RandomState(seed)
@@ -38,7 +39,10 @@ def test_loss_partial_decreasing(mode):
     W_list = rng.randn(m, p, p)
     S_list = np.array([W.dot(X) for W, X in zip(W_list, X_list)])
     S_avg = np.mean(S_list, axis=0)
-    tau_list = np.zeros(m, dtype="int")
+    if multiple_sources:
+        tau_list = np.zeros((m, p), dtype="int")
+    else:
+        tau_list = np.zeros(m, dtype="int")
 
     # loss over 20 iterations
     loss0 = loss_partial(W_list, S_list, S_avg)
@@ -48,14 +52,23 @@ def test_loss_partial_decreasing(mode):
     for i in range(nb_iter):
         # print(f"\nIteration {i}")
         # step 1: delay optimization
-        _, tau_list, _ = _optimization_tau_approach1(
-            S_list,
-            n_iter=3,
-            delay_max=delay_max,
-            previous_tau_list=tau_list
-        )
-        print("tau_list : ", tau_list)
-        Y_list = _apply_delay(S_list, -tau_list)
+        if multiple_sources:
+            tau_list = _optimization_tau_by_source(
+                S_list,
+                n_iter=3,
+                delay_max=delay_max,
+                previous_tau_list=tau_list
+            )
+            Y_list = _apply_delay_by_source(S_list, -tau_list)
+        else:
+            _, tau_list, _ = _optimization_tau_approach1(
+                S_list,
+                n_iter=3,
+                delay_max=delay_max,
+                previous_tau_list=tau_list
+            )
+            # print("tau_list : ", tau_list)
+            Y_list = _apply_delay(S_list, -tau_list)
         Y_avg = np.mean(Y_list, axis=0)
         loss1 = loss_partial(W_list, Y_list, Y_avg)
         print(f"loss1 : {loss1}")
