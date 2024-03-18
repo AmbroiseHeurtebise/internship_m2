9f46d514f0a73c0c6825e8006e41b94d9e26a169
Ambroise Heurtebise <amheurtebise@gmail.com>
2024-03-18 18:15:27 +0100

    add other functions that apply delay ; I will sort them out later

--- lbfgsb_mvica_ds/apply_dilations_shifts.py
+++ lbfgsb_mvica_ds/apply_dilations_shifts.py
@@ -110,16 +110,16 @@ def apply_both_delays_2d_cyclic(
 
 def apply_both_delays_3d_cyclic(
     S_list,
-    A,
-    B,
-    max_shift=0.,
+    dilations,
+    shifts,
     max_dilation=1.,
+    max_shift=0.,
     shift_before_dilation=True,
     n_concat=1,
 ):
     Y_list = jnp.array(
         [apply_both_delays_2d_cyclic(
-            S_list[i], a=A[i], b=B[i], max_shift=max_shift, max_dilation=max_dilation,
+            S_list[i], a=dilations[i], b=shifts[i], max_dilation=max_dilation, max_shift=max_shift,
             shift_before_dilation=shift_before_dilation, n_concat=n_concat)
          for i in range(len(S_list))])
     return Y_list
@@ -167,7 +167,9 @@ def apply_dilations_shifts_4d_jax(S, dilations, shifts, max_dilation=1., max_shi
     return S_ds
 
 
-def apply_dilations_shifts_3d_jax(S, dilations, shifts, max_dilation=1., max_shift=0., n_concat=1):
+def apply_dilations_shifts_3d_jax(
+    S, dilations, shifts, max_dilation=1., max_shift=0., shift_before_dilation=True, n_concat=1,
+):
     m, p, n_total = S.shape
     n = n_total // n_concat
     S_4d = jnp.moveaxis(jnp.array(jnp.split(S, n_concat, axis=-1)), source=0, destination=2)
@@ -178,7 +180,10 @@ def apply_dilations_shifts_3d_jax(S, dilations, shifts, max_dilation=1., max_shi
     T = jnp.array([t] * m * p * n_concat).reshape((m, p, n_concat, n))
     dilations_newaxis = dilations[:, :, jnp.newaxis, jnp.newaxis]
     shifts_newaxis = shifts[:, :, jnp.newaxis, jnp.newaxis]
-    T_ds = T * dilations_newaxis - shifts_newaxis  # corresponds to shift_before_dilation=False
+    if shift_before_dilation:
+        T_ds = (T - shifts_newaxis) * dilations_newaxis
+    else:
+        T_ds = T * dilations_newaxis - shifts_newaxis
     T_ds_ravel = T_ds.reshape((-1, n))
     S_extended = jnp.concatenate(
         [S_4d[:, :, :, n-max_delay_samples:], S_4d, S_4d[:, :, :, :max_delay_samples]], axis=-1)
@@ -186,3 +191,107 @@ def apply_dilations_shifts_3d_jax(S, dilations, shifts, max_dilation=1., max_shi
     S_ds = interp_mapped(T_ds_ravel, t_extended, S_extended_ravel)
     S_ds = S_ds.reshape(S.shape)
     return S_ds
+
+
+def apply_dilations_shifts_3d_no_argmin(
+    S, dilations, shifts, max_dilation=1., max_shift=0., shift_before_dilation=True, n_concat=1,
+):
+    m, p, n_total = S.shape
+    n = n_total // n_concat
+    S_4d = jnp.moveaxis(jnp.array(jnp.split(S, n_concat, axis=-1)), source=0, destination=2)
+    max_delay_time = (1 + max_shift) * max_dilation - 1
+    max_delay_samples = np.ceil(max_delay_time * n).astype("int")
+    t_extended = jnp.arange(n+2*max_delay_samples) - max_delay_samples
+    t = jnp.arange(n)
+    T = jnp.array([t] * m * p * n_concat).reshape(S_4d.shape)
+    dilations_newaxis = dilations[:, :, jnp.newaxis, jnp.newaxis]
+    shifts_newaxis = shifts[:, :, jnp.newaxis, jnp.newaxis] * n
+    if shift_before_dilation:
+        T_ds = (T - shifts_newaxis) * dilations_newaxis
+    else:
+        T_ds = T * dilations_newaxis - shifts_newaxis
+    # ind = jnp.rint(T_ds).astype(int) + max_delay_samples
+    # ind = jnp.clip(ind, 1, n+2*max_delay_samples-2)
+    # T_ref = t_extended[ind]
+    # T_ref = jnp.clip(jnp.rint(T_ds).astype(int), -max_delay_samples+1, n+max_delay_samples-2)
+    # # ind = jnp.clip(T_ref + max_delay_samples, 1, n+2*max_delay_samples-2)
+    # ind = T_ref + max_delay_samples
+    # T_ds_clipped = jnp.clip(T_ds, -max_delay_samples+1, n+max_delay_samples-2)
+    T_ds_clipped = jnp.clip(T_ds, -max_delay_samples+1, n+max_delay_samples-2)
+    T_ref = jnp.rint(T_ds).astype(int)
+    ind = T_ref + max_delay_samples
+    # jnp.copysign allows to avoid the case sign==0
+    signs = jnp.copysign(1, jnp.sign(T_ds_clipped - T_ref)).astype(int)
+    S_extended = jnp.concatenate(
+        [S_4d[:, :, :, n-max_delay_samples:], S_4d, S_4d[:, :, :, :max_delay_samples]], axis=-1)
+    S_extended_ind = jnp.take_along_axis(S_extended, ind, axis=-1)
+    t_extended_ind = t_extended[ind]
+    slopes = (jnp.take_along_axis(S_extended, ind+signs, axis=-1) - S_extended_ind) / (
+        t_extended[ind + signs] - t_extended_ind)
+    intercepts = S_extended_ind - slopes * t_extended_ind
+    S_ds = slopes * T_ds + intercepts
+    S_ds = S_ds.reshape((m, p, -1))
+    return S_ds
+
+
+def apply_dilations_shifts_4d_no_argmin(
+    S, dilations, shifts, max_dilation=1., max_shift=0., shift_before_dilation=True,
+):
+    m, p, n_concat, n = S.shape
+    max_delay_time = (1 + max_shift) * max_dilation - 1
+    max_delay_samples = np.ceil(max_delay_time * n).astype("int")
+    t_extended = jnp.arange(n+2*max_delay_samples) - max_delay_samples
+    t = jnp.arange(n)
+    T = jnp.array([t] * m * p * n_concat).reshape(S.shape)
+    dilations_newaxis = dilations[:, :, jnp.newaxis, jnp.newaxis]
+    shifts_newaxis = shifts[:, :, jnp.newaxis, jnp.newaxis] * n
+    if shift_before_dilation:
+        T_ds = (T - shifts_newaxis) * dilations_newaxis
+    else:
+        T_ds = T * dilations_newaxis - shifts_newaxis
+    # ind = jnp.rint(T_ds).astype(int) + max_delay_samples
+    # ind = jnp.clip(ind, 1, n+2*max_delay_samples-2)
+    # T_ref = t_extended[ind]
+    T_ref = jnp.clip(jnp.rint(T_ds).astype(int), -max_delay_samples+1, n+max_delay_samples-2)
+    ind = jnp.clip(T_ref + max_delay_samples, 1, n+2*max_delay_samples-2)
+    T_ds_clipped = jnp.clip(T_ds, -max_delay_samples+1, n+max_delay_samples-2)
+    # jnp.copysign allows to avoid the case sign==0
+    signs = jnp.copysign(1, jnp.sign(T_ds_clipped - T_ref)).astype(int)
+    S_extended = jnp.concatenate([S[:, :, :, n-max_delay_samples:], S, S[:, :, :, :max_delay_samples]], axis=-1)
+    S_extended_ind = jnp.take_along_axis(S_extended, ind, axis=-1)
+    t_extended_ind = t_extended[ind]
+    slopes = (jnp.take_along_axis(S_extended, ind+signs, axis=-1) - S_extended_ind) / (
+        t_extended[ind + signs] - t_extended_ind)
+    intercepts = S_extended_ind - slopes * t_extended_ind
+    S_ds = slopes * T_ds + intercepts
+    return S_ds
+
+
+def apply_dilations_shifts_1d(
+    s, dilation, shift, max_dilation=1., max_shift=0., shift_before_dilation=True, n_concat=1
+):
+    assert len(s) % n_concat == 0
+    S = np.array(np.split(s, n_concat))
+    n = S.shape[1]
+    max_delay_time = (1 + max_shift) * max_dilation - 1
+    max_delay_samples = np.ceil(max_delay_time * n).astype("int")
+    t_extended = jnp.arange(n+2*max_delay_samples) - max_delay_samples
+    t = jnp.arange(n)
+    shift *= n
+    if shift_before_dilation:
+        t_ds = (t - shift) * dilation
+    else:
+        t_ds = t * dilation - shift
+    t_ds_clipped = np.clip(t_ds, -max_delay_samples+1, n+max_delay_samples-2)
+    t_ref = t_ds_clipped.astype(int)
+    ind = (t_ref + max_delay_samples)[np.newaxis, :]
+    signs = np.copysign(1, np.sign(t_ds_clipped - t_ref)).astype(int)
+    S_extended = np.concatenate([S[:, n-max_delay_samples:], S, S[:, :max_delay_samples]], axis=-1)
+    S_extended_ind = np.take_along_axis(S_extended, ind, axis=1)
+    t_extended_ind = t_extended[ind]
+    slopes = (np.take_along_axis(S_extended, ind+signs, axis=1) - S_extended_ind) / (
+        t_extended[ind + signs] - t_extended_ind)
+    intercepts = S_extended_ind - slopes * t_extended_ind
+    S_ds = slopes * t_ds + intercepts
+    s_ds = S_ds.ravel()
+    return s_ds
