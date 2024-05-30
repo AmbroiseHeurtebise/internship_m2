7c406a7e04c4f07e5c0be08a07de70e777c9a5a4
AmbroiseHeurtebise <amheurtebise@gmail.com>
2024-05-30 19:20:54 +0200

    also plot shift and dilation's errors

--- tbme/plots/plot_results_penalization_scale.py
+++ tbme/plots/plot_results_penalization_scale.py
@@ -7,15 +7,15 @@ import seaborn as sns
 # read dataframe
 nb_seeds = 30
 results_dir = "/storage/store2/work/aheurteb/mvicad/tbme/data/"
-save_name = f"DataFrame_with_{nb_seeds}_by_penalization_scale"
+save_name = f"DataFrame_with_{nb_seeds}_seeds_wrt_penalization_scale"
 save_path = results_dir + save_name
 df = pd.read_csv(save_path)
 
-# plot
+# plot Amari distance
 prop_cycle = plt.rcParams['axes.prop_cycle']
 colors = prop_cycle.by_key()['color']
 
-fig = plt.figure(figsize=(6, 4))
+plt.figure(figsize=(6, 4))
 sns.lineplot(
     data=df, x="penalization scale", y="Amari MVICAD", linewidth=2.5,
     label="MVICAD", estimator=np.median, c=colors[1])
@@ -33,4 +33,16 @@ plt.grid()
 plt.title("Median Amari distance with respect to penalization scale")
 figures_dir = "/storage/store2/work/aheurteb/mvicad/tbme/figures/"
 plt.savefig(figures_dir + "amari_distance_wrt_penalization_scale.pdf")
+plt.show()
+
+# plot shift and dilation's errors
+plt.figure(figsize=(6, 4))
+sns.lineplot(data=df, x="penalization scale", y="Dilations error LBFGSB", linewidth=2.5, label="dilations error", estimator=np.median)
+sns.lineplot(data=df, x="penalization scale", y="Shifts error LBFGSB", linewidth=2.5, label="shifts error", estimator=np.median)
+plt.xscale("log")
+plt.yscale("log")
+plt.ylabel("Error")
+plt.grid()
+plt.title("Dilation and shift errors wrt penalization scale")
+plt.savefig(figures_dir + "dilation_shift_errors_wrt_penalization_scale.pdf")
 plt.show()
