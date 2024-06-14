3ab07597bbefa702e6eb2c81dca7153e70887bb8
AmbroiseHeurtebise <amheurtebise@gmail.com>
2024-06-14 02:47:34 +0200

    import PCA and allow to use only subgroups of subjects

--- tbme/runs/runs_camcan/run_camcan.py
+++ tbme/runs/runs_camcan/run_camcan.py
@@ -1,27 +1,30 @@
 import os
 import numpy as np
-from sklearn.utils.extmath import randomized_svd
 from multiviewica_delay import mvica_ds
+from multiviewica_delay.multiviewica_shifts._reduce_data import pca_reduce_data
 
 
-def load_data(task, nb_subjects):
-    data_dir = "/storage/store2/work/aheurteb/mvicad/tbme/data/camcan_old/"
+def load_data(task, n_subjects, n_concat):
+    data_dir = f"/storage/store2/work/aheurteb/mvicad/tbme/data/camcan/{task}/"
+    suffix1 = f"_{task}_task_mag_{n_subjects}_{n_concat}.npy"
+    suffix2 = f"_{task}_task_{n_subjects}_{n_concat}.npy"
     # Load dataset
-    dataset_name = f"X_{task}_task_mag_{nb_subjects}.npy"
+    dataset_name = "X" + suffix1
     dataset_path = data_dir + dataset_name
+    print(f"dataset path : {dataset_path}")
     if os.path.exists(dataset_path):
         X = np.load(dataset_path)
     else:
         raise NameError("Dataset not found.")
     # Save subjects
-    subjects_name = f"subjects_{task}_task_{nb_subjects}.npy"
+    subjects_name = "subjects" + suffix2
     subjects_path = data_dir + subjects_name
     if os.path.exists(subjects_path):
         subjects = np.load(subjects_path)
     else:
         raise NameError("Subjects not found.")
     # save ages
-    ages_name = f"ages_{task}_task_{nb_subjects}.npy"
+    ages_name = "ages" + suffix2
     ages_path = data_dir + ages_name
     if os.path.exists(ages_path):
         ages = np.load(ages_path)
@@ -30,35 +33,42 @@ def load_data(task, nb_subjects):
     return X, subjects, ages
 
 
-def pca_reduce_data(X, n_components, random_state=None):
-    n_groups, n_features, n_samples = X.shape
-    reduced = []
-    basis = []
-    for i in range(n_groups):
-        U_i, S_i, V_i = randomized_svd(
-            X[i], n_components=n_components, random_state=random_state)
-        reduced.append(S_i.reshape(-1, 1) * V_i)
-        basis.append(U_i.T)
-    return np.array(basis), np.array(reduced)
-
-
 # parameters
+task = "auditory"
+n_concat = 6
 n_components_pca = 5
-nb_subjects_subgroup = 50
+n_subjects_subgroup = 50
 max_dilation = 1.15
 max_shift = 0.05
 W_scale = 200
 random_state = 42
+
+# define n_subjects_data
+if task == "visual":
+    n_subjects_data = 477
+elif task == "auditory":
+    n_subjects_data = 501
+
 # load data
-X, subjects, ages = load_data("visual", 477)
+X, subjects, ages = load_data(task, n_subjects_data, n_concat)
+print(f"Dataset shape : {X.shape}")
+
+# eventually reduce the number of subjects
+if n_subjects_subgroup is not None:
+    X = X[:n_subjects_subgroup]
+    subjects = subjects[:n_subjects_subgroup]
+    ages = ages[:n_subjects_subgroup]
+
 # apply PCA
 P, X = pca_reduce_data(
     X, n_components=n_components_pca, random_state=random_state,
 )
+print(f"Dataset shape after PCA : {X.shape}")
+
 # MVICA_DS
 W_list, dilations, shifts, Y_list = mvica_ds(
     X_list=X,
-    n_concat=1,
+    n_concat=n_concat,
     max_dilation=max_dilation,
     max_shift=max_shift,
     dilation_scale_per_source=True,
@@ -76,12 +86,15 @@ W_list, dilations, shifts, Y_list = mvica_ds(
     return_all_iterations=False,
     S_list_true=None,
 )
+
 # save data
+if n_subjects_subgroup is None:
+    n_subjects_subgroup = n_subjects_data
 results_dir = "/storage/store2/work/aheurteb/mvicad/tbme/results/results_camcan/"
-W_save_name = "W_old_data.npy"
-dilations_save_name = "dilations_old_data.npy"
-shifts_save_name = "shifts_old_data.npy"
-Y_save_name = "Y_old_data.npy"
+W_save_name = f"W_{task}_task_{n_subjects_subgroup}_{n_components_pca}_{n_concat}.npy"
+dilations_save_name = f"dilations_{task}_task_{n_subjects_subgroup}_{n_components_pca}_{n_concat}.npy"
+shifts_save_name = f"shifts_{task}_task_{n_subjects_subgroup}_{n_components_pca}_{n_concat}.npy"
+Y_save_name = f"Y_{task}_task_{n_subjects_subgroup}_{n_components_pca}_{n_concat}.npy"
 np.save(results_dir + W_save_name, W_list)
 np.save(results_dir + dilations_save_name, dilations)
 np.save(results_dir + shifts_save_name, shifts)
