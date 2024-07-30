4a8e79f65a284ffb70474cb7dce232db6343afa4
AmbroiseHeurtebise <amheurtebise@gmail.com>
2024-07-30 06:09:19 +0200

    plot both figures side by side

--- tbme/plots/plots_synthetic_data/plot_results_wrt_W_scale.py
+++ tbme/plots/plots_synthetic_data/plot_results_wrt_W_scale.py
@@ -5,6 +5,43 @@ from matplotlib.font_manager import FontProperties
 import seaborn as sns
 
 
+def plot_figure_amari(df, ax, font_properties):
+    sns.lineplot(
+        data=df, x="W_scale", y="Amari LBFGSB", linewidth=2.5,
+        label="MVICAD$^2$", estimator=np.median, c=colors[0])
+    plt.xscale("log", base=2)
+    plt.yscale("log")
+    plt.xlabel("Parameter W_scale", font_properties=font_properties)
+    plt.ylabel("Median Amari distance", font_properties=font_properties)
+    for label in ax.get_xticklabels():
+        label.set_fontproperties(font_properties)
+    for label in ax.get_yticklabels():
+        label.set_fontproperties(font_properties)
+    ax.legend_.remove()
+    plt.grid()
+
+
+def plot_figure_errors(df, ax, font_properties):
+    sns.lineplot(
+        data=df, x="W_scale", y="Dilations error LBFGSB", linewidth=2.5,
+        label="dilations error", estimator=np.median, c=colors[0], linestyle=":",
+        marker="^")
+    sns.lineplot(
+        data=df, x="W_scale", y="Shifts error LBFGSB", linewidth=2.5,
+        label="shifts error", estimator=np.median, c=colors[0], linestyle="--",
+        marker="o")
+    plt.xscale("log", base=2)
+    plt.yscale("log")
+    plt.xlabel("Parameter W_scale", font_properties=font_properties)
+    plt.ylabel("Error", font_properties=font_properties)
+    for label in ax.get_xticklabels():
+        label.set_fontproperties(font_properties)
+    for label in ax.get_yticklabels():
+        label.set_fontproperties(font_properties)
+    plt.legend(prop=font_properties)
+    plt.grid()
+
+
 # read dataframe
 nb_seeds = 30
 m = 5
@@ -25,39 +62,20 @@ colors = prop_cycle.by_key()['color']
 
 # plot Amari distance
 fig, ax = plt.subplots(figsize=(6, 4))
-sns.lineplot(
-    data=df, x="W_scale", y="Amari LBFGSB", linewidth=2.5,
-    label="MVICAD$^2$", estimator=np.median, c=colors[0])
-plt.legend()
-plt.xscale("log", base=2)
-plt.yscale("log")
-plt.xlabel("Parameter W_scale", font_properties=font_properties)
-plt.ylabel("Median Amari distance", font_properties=font_properties)
-for label in ax.get_xticklabels():
-    label.set_fontproperties(font_properties)
-for label in ax.get_yticklabels():
-    label.set_fontproperties(font_properties)
-ax.legend_.remove()
-plt.grid()
+plot_figure_amari(df, ax, font_properties)
 figures_dir = "/storage/store2/work/aheurteb/mvicad/tbme/figures/"
 plt.savefig(figures_dir + "amari_distance_wrt_W_scale.pdf", bbox_inches="tight")
-plt.show()
 
 # plot shift and dilation's errors
 fig, ax = plt.subplots(figsize=(6, 4))
-sns.lineplot(
-    data=df, x="W_scale", y="Dilations error LBFGSB", linewidth=2.5,
-    label="dilations error", estimator=np.median, c=colors[0], linestyle=":",
-    marker="^")
-sns.lineplot(
-    data=df, x="W_scale", y="Shifts error LBFGSB", linewidth=2.5,
-    label="shifts error", estimator=np.median, c=colors[0], linestyle="--",
-    marker="o")
-plt.xscale("log", base=2)
-plt.yscale("log")
-plt.xlabel("Parameter W_scale", font_properties=font_properties)
-plt.ylabel("Error", font_properties=font_properties)
-plt.grid()
-# plt.subplots_adjust(bottom=0.2)
+plot_figure_errors(df, ax, font_properties)
 plt.savefig(figures_dir + "dilation_shift_errors_wrt_W_scale.pdf", bbox_inches="tight")
-plt.show()
+
+# plot both figures side by side
+fig, axes = plt.subplots(1, 2, figsize=(12, 4))
+plt.subplot(1, 2, 1)
+plot_figure_amari(df, axes[0], font_properties)
+plt.subplot(1, 2, 2)
+plot_figure_errors(df, axes[1], font_properties)
+plt.tight_layout(pad=1.5, w_pad=4.0)
+plt.savefig(figures_dir + "both_figures_wrt_W_scale.pdf", bbox_inches="tight")
