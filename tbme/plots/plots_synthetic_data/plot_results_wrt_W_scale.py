f536face2a6865ef976c22f879aaa4ba1cdc04c5
AmbroiseHeurtebise <amheurtebise@gmail.com>
2024-07-30 06:22:46 +0200

    common xlabel

--- tbme/plots/plots_synthetic_data/plot_results_wrt_W_scale.py
+++ tbme/plots/plots_synthetic_data/plot_results_wrt_W_scale.py
@@ -5,13 +5,16 @@ from matplotlib.font_manager import FontProperties
 import seaborn as sns
 
 
-def plot_figure_amari(df, ax, font_properties):
+def plot_figure_amari(df, ax, font_properties, xlabel=True):
     sns.lineplot(
         data=df, x="W_scale", y="Amari LBFGSB", linewidth=2.5,
         label="MVICAD$^2$", estimator=np.median, c=colors[0])
     plt.xscale("log", base=2)
     plt.yscale("log")
-    plt.xlabel("Parameter W_scale", font_properties=font_properties)
+    if xlabel:
+        plt.xlabel("Parameter W_scale", font_properties=font_properties)
+    else:
+        plt.xlabel("")
     plt.ylabel("Median Amari distance", font_properties=font_properties)
     for label in ax.get_xticklabels():
         label.set_fontproperties(font_properties)
@@ -21,7 +24,7 @@ def plot_figure_amari(df, ax, font_properties):
     plt.grid()
 
 
-def plot_figure_errors(df, ax, font_properties):
+def plot_figure_errors(df, ax, font_properties, xlabel=True):
     sns.lineplot(
         data=df, x="W_scale", y="Dilations error LBFGSB", linewidth=2.5,
         label="dilations error", estimator=np.median, c=colors[0], linestyle=":",
@@ -32,7 +35,10 @@ def plot_figure_errors(df, ax, font_properties):
         marker="o")
     plt.xscale("log", base=2)
     plt.yscale("log")
-    plt.xlabel("Parameter W_scale", font_properties=font_properties)
+    if xlabel:
+        plt.xlabel("Parameter W_scale", font_properties=font_properties)
+    else:
+        plt.xlabel("")
     plt.ylabel("Error", font_properties=font_properties)
     for label in ax.get_xticklabels():
         label.set_fontproperties(font_properties)
@@ -74,8 +80,9 @@ plt.savefig(figures_dir + "dilation_shift_errors_wrt_W_scale.pdf", bbox_inches="
 # plot both figures side by side
 fig, axes = plt.subplots(1, 2, figsize=(12, 4))
 plt.subplot(1, 2, 1)
-plot_figure_amari(df, axes[0], font_properties)
+plot_figure_amari(df, axes[0], font_properties, xlabel=False)
 plt.subplot(1, 2, 2)
-plot_figure_errors(df, axes[1], font_properties)
-plt.tight_layout(pad=1.5, w_pad=4.0)
+plot_figure_errors(df, axes[1], font_properties, xlabel=False)
+fig.supxlabel("Parameter W_scale", fontsize=fontsize, font_properties=font_properties)
+plt.tight_layout(pad=0.5, w_pad=4.0)
 plt.savefig(figures_dir + "both_figures_wrt_W_scale.pdf", bbox_inches="tight")
