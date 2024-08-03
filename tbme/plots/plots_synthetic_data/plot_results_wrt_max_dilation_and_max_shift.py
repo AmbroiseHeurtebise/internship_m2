add375fbf183ac787af907926a019970c239d126
AmbroiseHeurtebise <amheurtebise@gmail.com>
2024-08-03 22:40:41 +0200

    add MVICA and GroupICA, change font

--- tbme/plots/plots_synthetic_data/plot_results_wrt_max_dilation_and_max_shift.py
+++ tbme/plots/plots_synthetic_data/plot_results_wrt_max_dilation_and_max_shift.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.text as mpl_text
+from matplotlib.font_manager import FontProperties
 import seaborn as sns
 
 
@@ -18,7 +19,7 @@ class AnyObjectHandler(object):
             x=0, y=0, text=orig_handle.my_text, color=orig_handle.my_color, verticalalignment=u'baseline',
             horizontalalignment=u'left', multialignment=None,
             fontproperties=None, rotation=0, linespacing=None,
-            rotation_mode=None)
+            rotation_mode=None, font_properties=font_properties)
         handlebox.add_artist(patch)
         return patch
 
@@ -30,25 +31,32 @@ save_name = f"DataFrame_with_{nb_seeds}_seeds_wrt_max_dilation_and_max_shift"
 save_path = results_dir + save_name
 df = pd.read_csv(save_path)
 
+# get Times New Roman font
+fontsize = 12
+font_path = "/storage/store2/work/aheurteb/mvicad/tbme/fonts/Times_New_Roman.ttf"
+font_properties = FontProperties(fname=font_path, size=fontsize)
+
 # colors
 prop_cycle = plt.rcParams['axes.prop_cycle']
 colors = prop_cycle.by_key()['color']
 
 # plot the Amari distance
-plt.figure(figsize=(6, 4))
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(figsize=(6, 4))
 sns.lineplot(
-    data=df, x="max_dilation", y="Amari MVICAD", linewidth=2.5,
-    label="MVICAD", estimator=np.median, c=colors[1])
+    data=df, x="max_dilation", y="Amari GroupICA", linewidth=2.5,
+    label="GroupICA", estimator=np.median, c=colors[4])
 sns.lineplot(
-    data=df, x="max_dilation", y="Amari MVICAD ext", linewidth=2.5,
-    label="MVICAD extended", estimator=np.median, c=colors[2])
+    data=df, x="max_dilation", y="Amari MVICA", linewidth=2.5,
+    label="MVICA", estimator=np.median, c=colors[2])
 sns.lineplot(
     data=df, x="max_dilation", y="Amari permica", linewidth=2.5,
     label="PermICA", estimator=np.median, c=colors[3])
 sns.lineplot(
+    data=df, x="max_dilation", y="Amari MVICAD ext", linewidth=2.5,
+    label="MVICAD", estimator=np.median, c=colors[1])
+sns.lineplot(
     data=df, x="max_dilation", y="Amari LBFGSB", linewidth=2.5,
-    label="LBFGSB", estimator=np.median, c=colors[0])
+    label="MVICAD$^2$", estimator=np.median, c=colors[0])
 # xticks
 max_dilation_all = np.unique(df["max_dilation"])
 max_shift_all = np.unique(df["max_shift"])
@@ -64,25 +72,27 @@ for i, tick in enumerate(xticks):
         label_parts = xticklabels[i].split()
         for j, part in enumerate(label_parts):
             ax.text(
-                x=tick+0.013*(j-1), y=5.45*1e-5, s=part, transform=ax.transData,
-                color=xticklabels_colors[j], fontsize=10, rotation=0, ha="center")
+                x=tick+0.013*(j-1), y=3*1e-5, s=part, transform=ax.transData,
+                color=xticklabels_colors[j], fontsize=10, rotation=0, ha="center",
+                font_properties=font_properties)
+# yticklabels
+for label in ax.get_yticklabels():
+    label.set_fontproperties(font_properties)
 # legend
-leg1 = ax.legend(loc="upper left")
+leg1 = ax.legend(loc="upper left", prop=font_properties)
 ax.add_artist(leg1)
 obj_0 = AnyObject("1", xticklabels_colors[0])
 obj_1 = AnyObject("2", xticklabels_colors[2])
 ax.legend(
     [obj_0, obj_1], ["max_dilation", "max_shift"],
     handler_map={obj_0: AnyObjectHandler(), obj_1: AnyObjectHandler()},
-    bbox_to_anchor=(0.99, -0.08))
+    bbox_to_anchor=(0.99, -0.08), prop=font_properties)
 # other features
 plt.yscale("log")
-plt.xlabel("Maximum dilation and maximum shift")
-ax.xaxis.set_label_coords(0.35, -0.14)
-plt.ylabel("Amari distance")
-plt.title("Median Amari distance with respect to max_dilation and max_shift")
+ax.set_xlabel("Maximum dilation and maximum shift", font_properties=font_properties)
+ax.xaxis.set_label_coords(0.35, -0.165)
+ax.set_ylabel("Median Amari distance", font_properties=font_properties)
 plt.grid()
-plt.subplots_adjust(bottom=0.2)
 figures_dir = "/storage/store2/work/aheurteb/mvicad/tbme/figures/"
-plt.savefig(figures_dir + "amari_distance_wrt_max_dilation_and_max_shift.pdf")
+plt.savefig(figures_dir + "amari_distance_wrt_max_dilation_and_max_shift.pdf", bbox_inches="tight")
 plt.show()
