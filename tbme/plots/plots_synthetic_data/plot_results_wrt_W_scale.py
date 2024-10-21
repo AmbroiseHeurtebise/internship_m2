426fce075267c77d40996fc29652236fec5f344f
AmbroiseHeurtebise <amheurtebise@gmail.com>
2024-10-22 00:59:48 +0200

    update figures dimensions

--- tbme/plots/plots_synthetic_data/plot_results_wrt_W_scale.py
+++ tbme/plots/plots_synthetic_data/plot_results_wrt_W_scale.py
@@ -15,7 +15,7 @@ def plot_figure_amari(df, ax, font_properties, xlabel=True):
         plt.xlabel(r"Parameter $\Lambda^2$", font_properties=font_properties)
     else:
         plt.xlabel("")
-    plt.ylabel("Median Amari distance", font_properties=font_properties)
+    plt.ylabel("Median Amari\ndistance", font_properties=font_properties)
     for label in ax.get_xticklabels():
         label.set_fontproperties(font_properties)
     for label in ax.get_yticklabels():
@@ -44,7 +44,9 @@ def plot_figure_errors(df, ax, font_properties, xlabel=True):
         label.set_fontproperties(font_properties)
     for label in ax.get_yticklabels():
         label.set_fontproperties(font_properties)
-    plt.legend(prop=font_properties)
+    plt.legend(
+        prop=font_properties, loc="upper right", bbox_to_anchor=(0.96, 1.07),
+        handlelength=1.0)
     plt.grid()
 
 
@@ -58,7 +60,7 @@ save_path = results_dir + save_name
 df = pd.read_csv(save_path)
 
 # get Times New Roman font
-fontsize = 18
+fontsize = 26
 font_path = "/storage/store2/work/aheurteb/mvicad/tbme/fonts/Times_New_Roman.ttf"
 font_properties = FontProperties(fname=font_path, size=fontsize)
 
@@ -66,23 +68,25 @@ font_properties = FontProperties(fname=font_path, size=fontsize)
 prop_cycle = plt.rcParams['axes.prop_cycle']
 colors = prop_cycle.by_key()['color']
 
-# plot Amari distance
-fig, ax = plt.subplots(figsize=(6, 3.5))
-plot_figure_amari(df, ax, font_properties)
 figures_dir = "/storage/store2/work/aheurteb/mvicad/tbme/figures/"
-plt.savefig(figures_dir + "amari_distance_wrt_W_scale.pdf", bbox_inches="tight")
 
-# plot shift and dilation's errors
-fig, ax = plt.subplots(figsize=(6, 3.5))
-plot_figure_errors(df, ax, font_properties)
-plt.savefig(figures_dir + "dilation_shift_errors_wrt_W_scale.pdf", bbox_inches="tight")
+# # plot Amari distance
+# fig, ax = plt.subplots(figsize=(6, 3.5))
+# plot_figure_amari(df, ax, font_properties)
+# plt.savefig(figures_dir + "amari_distance_wrt_W_scale.pdf", bbox_inches="tight")
+
+# # plot shift and dilation's errors
+# fig, ax = plt.subplots(figsize=(6, 3.5))
+# plot_figure_errors(df, ax, font_properties)
+# plt.savefig(figures_dir + "dilation_shift_errors_wrt_W_scale.pdf", bbox_inches="tight")
 
 # plot both figures side by side
-fig, axes = plt.subplots(1, 2, figsize=(12, 3.5))
+fig, axes = plt.subplots(1, 2, figsize=(11, 3.3))
 plt.subplot(1, 2, 1)
 plot_figure_amari(df, axes[0], font_properties, xlabel=False)
 plt.subplot(1, 2, 2)
 plot_figure_errors(df, axes[1], font_properties, xlabel=False)
-fig.supxlabel(r"Parameter $\Lambda^2$", fontsize=fontsize, font_properties=font_properties)
-plt.tight_layout(pad=0.5, w_pad=4.0)
+plt.subplots_adjust(wspace=0.35)
+label = fig.supxlabel(r"Parameter $\Lambda^2$", fontsize=fontsize, font_properties=font_properties)
+label.set_position((0.5, -0.155))
 plt.savefig(figures_dir + "both_figures_wrt_W_scale.pdf", bbox_inches="tight")
--- tbme/plots/plots_synthetic_data/plot_results_wrt_penalization_scale.py
+++ tbme/plots/plots_synthetic_data/plot_results_wrt_penalization_scale.py
@@ -13,7 +13,7 @@ save_path = results_dir + save_name
 df = pd.read_csv(save_path)
 
 # get Times New Roman font
-fontsize = 18
+fontsize = 26
 font_path = "/storage/store2/work/aheurteb/mvicad/tbme/fonts/Times_New_Roman.ttf"
 font_properties = FontProperties(fname=font_path, size=fontsize)
 
@@ -22,14 +22,16 @@ prop_cycle = plt.rcParams['axes.prop_cycle']
 colors = prop_cycle.by_key()['color']
 
 # plot Amari distance
-fig, ax = plt.subplots(figsize=(6, 3.5))
+fig, ax = plt.subplots(figsize=(5, 3.3))
 sns.lineplot(
     data=df, x="penalization_scale", y="Amari LBFGSB", linewidth=2.5,
     label="MVICAD$^2$", estimator=np.median, c=colors[0])
 plt.xscale("log")
 plt.yscale("log")
 plt.xlabel("Parameter $\\lambda$", font_properties=font_properties)
-plt.ylabel("Median Amari distance", font_properties=font_properties)
+plt.ylabel("Median Amari\ndistance", font_properties=font_properties)
+xticks = np.logspace(-7, 5, 4)
+ax.set_xticks(xticks)
 for label in ax.get_xticklabels():
     label.set_fontproperties(font_properties)
 for label in ax.get_yticklabels():
@@ -42,28 +44,28 @@ plt.savefig(
     bbox_inches="tight")
 plt.show()
 
-# plot shift and dilation's errors
-fig, ax = plt.subplots(figsize=(6, 3.5))
-sns.lineplot(
-    data=df, x="penalization_scale", y="Dilations error LBFGSB", linewidth=2.5,
-    label="dilations error", estimator=np.median, c=colors[0], linestyle=":",
-    marker="^")
-sns.lineplot(
-    data=df, x="penalization_scale", y="Shifts error LBFGSB", linewidth=2.5,
-    label="shifts error", estimator=np.median, c=colors[0], linestyle="--",
-    marker="o")
-plt.xscale("log")
-plt.yscale("log")
-plt.xlabel("Parameter $\\lambda$", font_properties=font_properties)
-plt.ylabel("Error", font_properties=font_properties)
-ax.set_yticklabels([], minor=True)
-for label in ax.get_xticklabels():
-    label.set_fontproperties(font_properties)
-for label in ax.get_yticklabels():
-    label.set_fontproperties(font_properties)
-plt.legend(prop=font_properties)
-plt.grid()
-plt.savefig(
-    figures_dir + "dilation_shift_errors_wrt_penalization_scale.pdf",
-    bbox_inches="tight")
-plt.show()
+# # plot shift and dilation's errors
+# fig, ax = plt.subplots(figsize=(6, 3.5))
+# sns.lineplot(
+#     data=df, x="penalization_scale", y="Dilations error LBFGSB", linewidth=2.5,
+#     label="dilations error", estimator=np.median, c=colors[0], linestyle=":",
+#     marker="^")
+# sns.lineplot(
+#     data=df, x="penalization_scale", y="Shifts error LBFGSB", linewidth=2.5,
+#     label="shifts error", estimator=np.median, c=colors[0], linestyle="--",
+#     marker="o")
+# plt.xscale("log")
+# plt.yscale("log")
+# plt.xlabel("Parameter $\\lambda$", font_properties=font_properties)
+# plt.ylabel("Error", font_properties=font_properties)
+# ax.set_yticklabels([], minor=True)
+# for label in ax.get_xticklabels():
+#     label.set_fontproperties(font_properties)
+# for label in ax.get_yticklabels():
+#     label.set_fontproperties(font_properties)
+# plt.legend(prop=font_properties)
+# plt.grid()
+# plt.savefig(
+#     figures_dir + "dilation_shift_errors_wrt_penalization_scale.pdf",
+#     bbox_inches="tight")
+# plt.show()
